328958b309360a64cd8dbdaf8c27e6f1548f452b
Dasutein <dustine_2008@hotmail.com>
2019-04-12 20:28:11 +0800

    Revert "Re-organized files and folders"
    This reverts commit ee873a684a5f70cab2c4ce1949f30f2b5023f017.

--- js/extensionVersion.js
+++
@@ -1,9 +0,0 @@
-var requestJson = new XMLHttpRequest();
-requestJson.open('GET', 'manifest.json');
-requestJson.responseType = 'json';
-requestJson.send();
-
-function version(jsonObj){
-    console.log("extension_version:" + requestJson.response.version);
-    return requestJson.response.version;
-}
