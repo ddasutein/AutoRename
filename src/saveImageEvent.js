27602b66df444344ea2fe6c646acca124f8e1fc4
Dasutein <dustine_2008@hotmail.com>
2019-05-04 19:43:27 +0800

    New extension options function (saveImageEvent.js)
    Forgot to include with previous commit.

--- src/saveImageEvent.js
+++ src/saveImageEvent.js
@@ -6,7 +6,7 @@ var contextMenuItem = {
 
 chrome.contextMenus.create(contextMenuItem);
 
-function getRandomStrings(length) {
+function generateRandomString(length) {
     const value = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const random = [];
     for (let i = 0; i < length; i++) {
@@ -15,33 +15,63 @@ function getRandomStrings(length) {
     return random.join('');
 }
 
-const __randomStringLength = 8;
 var __extJPEG = ".jpg";
-var __extPNG = ".png"
+var __extPNG = ".png";
 
-chrome.contextMenus.onClicked.addListener(function(info, tab){
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
 
-    var tweetUrl = tab.url;
-    var tweetUrlSplit = tweetUrl.split('/');
-    var twitterWebsite = tweetUrlSplit[2];
-    var twitterUsername = tweetUrlSplit[3];
-    var tweetId = tweetUrlSplit[5];
+    // options.html and options.js
+    chrome.storage.local.get({
+        fileNameStringLength: '8',
+        showMentionSymbol: true,
+        showTweetId: true
+    }, function (items) {
 
-    if (twitterWebsite !== "twitter.com"){
-        alert("Sorry, this extension only supports Twitter at this time.");
-        return;
-    }
+        var _fileName = "";
+        var tweetUrl = tab.url;
+        var tweetUrlSplit = tweetUrl.split('/');
+        var twitterWebsite = tweetUrlSplit[2];
+        var twitterUsername = tweetUrlSplit[3];
+        var tweetId = tweetUrlSplit[5];
 
-    if (tweetId == null){
-        alert("Click on the tweet to use this extension.");
-        return;
-    }
-    else {
-        chrome.downloads.download({
-            url: info.srcUrl,
-            filename: "@" + twitterUsername + " [" + tweetId + "]" + "[" + getRandomStrings(__randomStringLength) + "]" + __extJPEG,
-            saveAs: true
-        })
-    }
+        if (items.showTweetId === true) {
 
+            if (items.showMentionSymbol === true) {
+                _fileName = "@" + twitterUsername + "-" + tweetId + "-" + generateRandomString(items.fileNameStringLength) + __extJPEG;
+            } else if (items.showMentionSymbol === false) {
+                _fileName = twitterUsername + "-" + tweetId + "-" + generateRandomString(items.fileNameStringLength) + __extJPEG;
+            }
+
+        } else if (items.showTweetId === false) {
+
+            if (items.showMentionSymbol === true) {
+                _fileName = "@" + twitterUsername + "-" + generateRandomString(items.fileNameStringLength) + __extJPEG;
+            } else if (items.showMentionSymbol === false) {
+                name = twitterUsername + "-" + generateRandomString(items.fileNameStringLength) + __extJPEG;
+            }
+
+        }
+
+        // Just generate a random string if both options are disabled (required)
+        if (items.showTweetId === false && items.showMentionSymbol === false) {
+            _fileName = generateRandomString(items.fileNameStringLength) + __extJPEG;
+        }
+
+        if (twitterWebsite !== "twitter.com") {
+            alert("Sorry, this extension only supports Twitter at this time.");
+            return;
+        }
+
+        if (tweetId == null) {
+            alert("Click on the tweet to use this extension.");
+            return;
+        } else {
+            chrome.downloads.download({
+                url: info.srcUrl,
+                filename: _fileName,
+                // filename: "@" + twitterUsername + " [" + tweetId + "]" + "[" + generateRandomString(__randomStringLength) + "]" + __extJPEG,
+                saveAs: true
+            })
+        }
+    });
 });
