2bfe2d826c70984a131be4db25a1634569ac9199
Dasutein <dustine_2008@hotmail.com>
2019-05-04 19:40:02 +0800

    New extension options function

---
+++ src/options.js
@@ -0,0 +1,29 @@
+function save_options(){
+
+    var fileNameStringLength = document.getElementById('twitter_string_length').value;
+    var includeMentionSymbol = document.getElementById('twitter_mention').checked;
+    var includeTweetId = document.getElementById('twitter_tweet_id').checked;
+
+    chrome.storage.local.set({
+        fileNameStringLength: fileNameStringLength,
+        showMentionSymbol: includeMentionSymbol,
+        showTweetId: includeTweetId
+    }, function(){
+        alert("Preferences saved");
+    });
+}
+
+function load_options(){
+    chrome.storage.local.get({
+        fileNameStringLength: '8',
+        showMentionSymbol: true,
+        showTweetId: true
+    }, function(items) {
+        document.getElementById('twitter_string_length').value = items.fileNameStringLength;
+        document.getElementById('twitter_mention').checked = items.showMentionSymbol;
+        document.getElementById('twitter_tweet_id').checked = items.showTweetId;
+    });
+}
+document.addEventListener('DOMContentLoaded', load_options);
+document.getElementById('buttonSaveTwitterPref').addEventListener('click',
+    save_options);
