044b1efd4b83865b2d17a6e08c33b1c42b624d3d
Dasutein <dustine_2008@hotmail.com>
2019-05-15 19:33:44 +0800

    Improvement to JSON parsing by enabling Synchronous and asynchronous requests

--- js/extensionInfo.js
+++ js/extensionInfo.js
@@ -1,22 +1,24 @@
-// GET info from manifest.json
+// Reference: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
 var request = new XMLHttpRequest();
-request.open('GET', chrome.extension.getURL('manifest.json'), false);
+
+request.onload = function(e) {
+  if (request.readyState === 4){
+    if (request.readyState === 200){
+      console.log(request.responseText);
+    } else{
+      var jsonConfig = this.responseText;
+      var jsonParse = JSON.parse(jsonConfig);
+      document.getElementById("extension_name").innerHTML = jsonParse.name;
+      document.getElementById("extension_version").innerHTML = 'Version: ' + jsonParse.version;
+      document.getElementById("extension_description").innerHTML = jsonParse.description;
+      console.log(request.statusText);
+    }
+  }
+}
+request.open('GET', chrome.extension.getURL('manifest.json'), true);
+request.onerror = function (e) {
+  console.error(request.statusText);
+};
+
 request.send(null);
-var manifest = JSON.parse(request.responseText);
 
-
-function loadExtensionVersion() {
-  document.getElementById("extension_version").innerHTML = 'Version: ' + manifest.version;
-}
-
-function loadExtensionName() {
-  document.getElementById("extension_name").innerHTML = manifest.name;
-}
-
-function loadExtensionDescription() {
-  document.getElementById("extension_description").innerHTML = manifest.name;
-}
-
-document.addEventListener('DOMContentLoaded', loadExtensionVersion);
-document.addEventListener('DOMContentLoaded', loadExtensionName);
-document.addEventListener('DOMContentLoaded', loadExtensionDescription);
