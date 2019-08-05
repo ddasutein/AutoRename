4acbae65a7319953d68c0489edc53f503f8f8b66
Dasutein <dustine_2008@hotmail.com>
2019-08-05 16:05:58 +0800

    Add date formatting
    New option to include the current date upon saving the image file.

--- js/saveImageEvent.js
+++ js/saveImageEvent.js
@@ -1,76 +1,149 @@
-let contextMenuItem = {
-    "id": "saveImage",
+chrome.contextMenus.create({
+    "id": "saveImage,",
     "title": "Save image as (AutoRename)",
-    "contexts": ["image"],
-};
+    "contexts": ["image"]
+});
 
-chrome.contextMenus.create(contextMenuItem);
-
-function generateRandomString(length) {
-    const value = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function GenerateRandomString(length) {
+    const value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const random = [];
     for (let i = 0; i < length; i++) {
         random.push(value[Math.floor(Math.random() * value.length)]);
     }
-    return random.join('');
+    return random.join("");
 }
 
-let _fileName = {
+/* Vanilla JavaScript doesn"t know how to count months 🙃
+[0] - January 
+[1] - February
+[2] - March
+[3] - April
+[4] - May
+[5] - June
+[6] - July
+[7] - August
+[8] - September
+[9] - October
+[10] - November
+[11] - December */
+let MonthsInNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+let MonthsInLong = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "13"];
+let MonthsInShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "13"];
+
+/* Time & Date */
+function GetDateFormat(DateTimeFormat) {
+
+    const _date = new Date();
+    let _finalTimeDateValue = "";
+
+    switch (DateTimeFormat) {
+        // 0-2 = Numerical Format
+        case "0":
+            _finalTimeDateValue = MonthsInNumber[_date.getMonth()] + "-" + _date.getDate() + "-" + _date.getFullYear();
+            break;
+        case "1":
+            _finalTimeDateValue = _date.getDate() + "-" + MonthsInNumber[_date.getMonth()] + "-" + _date.getFullYear();
+            break;
+        case "2":
+            _finalTimeDateValue = _date.getFullYear() + "-" + MonthsInNumber[_date.getMonth()] + "-" + _date.getDate();
+            break;
+            // 3-5 = Long Format
+        case "3":
+            _finalTimeDateValue = MonthsInLong[_date.getMonth()] + " " + _date.getDate() + ", " + _date.getFullYear();
+            break;
+        case "4":
+            _finalTimeDateValue = _date.getDate() + " " + MonthsInLong[_date.getMonth()] + " " + _date.getFullYear();
+            break;
+        case "5":
+            _finalTimeDateValue = _date.getFullYear() + " " + MonthsInLong[_date.getMonth()] + " " + _date.getDate();
+            break;
+            // 6-8 = Short Format
+        case "6":
+            _finalTimeDateValue = MonthsInShort[_date.getMonth()] + ". " + _date.getDate() + ", " + _date.getFullYear();
+            break;
+        case "7":
+            _finalTimeDateValue = _date.getDate() + " " + MonthsInShort[_date.getMonth()] + ". " + _date.getFullYear();
+            break;
+        case "8":
+            _finalTimeDateValue = _date.getFullYear() + " " + MonthsInShort[_date.getMonth()] + ". " + _date.getDate();
+            break;
+        default:
+            _finalTimeDateValue = null;
+            break;
+    }
+    return " (" + _finalTimeDateValue + ")";
+}
+
+/* Paramaters to build file name*/
+let FileNameBuilder = {
     username: "",
     tweetId: "",
     randomString: "",
-    fileExtension: ""
+    fileExtension: "",
+    timeanddate: ""
 };
 
-function fileName() {
-    const username = _fileName.username;
-    const tweetid = _fileName.tweetId;
-    const randomString = _fileName.randomString;
-    const fileExtension = _fileName.fileExtension;
+/* Setup file name to return final file name */
+function FileName() {
+    const username = FileNameBuilder.username;
+    const tweetid = FileNameBuilder.tweetId;
+    const randomString = FileNameBuilder.randomString;
+    const fileExtension = FileNameBuilder.fileExtension;
+    const _timeanddate = FileNameBuilder.timeanddate;
     let finalFileName = "";
 
     if (tweetid === "") {
-        finalFileName = username + "-" + randomString + fileExtension;
+        finalFileName = username + "-" + randomString + _timeanddate + fileExtension;
     } else {
-        finalFileName = username + "-" + tweetid + "-" + randomString + fileExtension;
+        finalFileName = username + "-" + tweetid + "-" + randomString + _timeanddate + fileExtension;
     }
     return finalFileName;
 }
 
+/* Execute everything when save image as is clicked. */
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
 
     let currentUrl = tab.url;
-    let currentUrlSplit = currentUrl.split('/');
+    let currentUrlSplit = currentUrl.split("/");
     let currentWebsite = currentUrlSplit[2];
 
     // options.html and options.js
     chrome.storage.local.get({
-        fileNameStringLength: '8',
+        fileNameStringLength: "8",
         showMentionSymbol: true,
         showTweetId: true,
-        twitterFileExtensionType : '.jpg'
+        twitterFileExtensionType: ".jpg",
+        useDate: false,
+        dateFormatting: "0"
     }, function (items) {
 
         switch (currentWebsite) {
             case "twitter.com":
+
                 const twitterUsername = currentUrlSplit[3];
                 const tweetId = currentUrlSplit[5];
 
-                _fileName.fileExtension = items.twitterFileExtensionType;
+                FileNameBuilder.fileExtension = items.twitterFileExtensionType;
 
                 if (!items.showMentionSymbol) {
-                    _fileName.username = twitterUsername;
+                    FileNameBuilder.username = twitterUsername;
                 } else {
-                    _fileName.username = "@" + twitterUsername;
+                    FileNameBuilder.username = "@" + twitterUsername;
                 }
 
                 if (!items.showTweetId) {
-                    _fileName.tweetId = "";
+                    FileNameBuilder.tweetId = "";
                 } else {
-                    _fileName.tweetId = tweetId;
+                    FileNameBuilder.tweetId = tweetId;
                 }
 
-                _fileName.randomString = generateRandomString(items.fileNameStringLength);
+                if (!items.useDate) {
+                    FileNameBuilder.timeanddate = "";
+                } else {
+                    FileNameBuilder.timeanddate = GetDateFormat(items.dateFormatting);
+                }
+
+                FileNameBuilder.randomString = GenerateRandomString(items.fileNameStringLength);
 
                 if (tweetId == null) {
                     alert("Click on the tweet to use this extension.");
@@ -78,13 +151,14 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
                 } else {
                     chrome.downloads.download({
                         url: info.srcUrl,
-                        filename: fileName(),
+                        filename: FileName(),
                         saveAs: true
                     })
                 }
                 break;
             default:
                 alert("Sorry, this extension only supports Twitter at this time. For a list of supported websites, visit my Github repository: https://github.com/ddasutein/AutoRename");
+                break;
         }
     });
 });
