d14b7ea3708671038ed63c8e93d6b74a400b6add
ddasutein <dasutein@outlook.com>
2020-04-11 17:23:47 +0800

    Create Debugger.js

---
+++ js/Common/Debugger.js
@@ -0,0 +1,75 @@
+/** MIT License
+ * 
+ * Copyright (c) 2020 Dasutein
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * See more: https://github.com/ddasutein/AutoRename/blob/master/LICENSE
+ * 
+ */
+
+console.log("Welcome to AutoRename" + "\n" +
+    "GitHub: " + "https://github.com/ddasutein/AutoRename" + "\n" +
+    "Enable Console Debugging by setting 'DevMode' to TRUE");
+
+let DevMode = true;
+
+let Debug = {
+
+    GetPlatformInfo: function () {
+        chrome.runtime.getPlatformInfo(function (info) {
+            console.log(info);
+        });
+    },
+
+    Settings: function (command) {
+    
+        LoadSettings();
+
+        let arrResult;
+
+        switch (command) {
+            case "all":
+                return SettingsArray;
+
+            case "general":
+                arrResult = SettingsArray.filter(function (key) {
+                    return key.category == CategoryEnum.General;
+                });
+
+                return arrResult;
+
+            case "twitter":
+                arrResult = SettingsArray.filter(function (key) {
+                    return key.category == CategoryEnum.Twitter;
+                });
+
+                return arrResult;
+
+            case "lineblog":
+                arrResult = SettingsArray.filter(function (key) {
+                    return key.category == CategoryEnum.LINE_BLOG;
+                });
+
+                return arrResult;
+        }
+
+    },
+}
+
+/**
+ * Output to Browser Console
+ * 
+ * @param {string} string 
+ */
+function WriteToConsole(string){
+    if (!DevMode){
+        console.log("<DEV MODE NOT ENABLED>");
+    } else {
+        console.log(string);
+    }
+}
