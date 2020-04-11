8b27a2552ff31178d4f07e99eee9c4d9e0592fbf
ddasutein <dasutein@outlook.com>
2020-04-11 15:02:38 +0800

    Add LICENSE header and removal of old code

--- js/Localization/i18n.js
+++ js/Localization/i18n.js
@@ -1,3 +1,17 @@
+/** MIT License
+ * 
+ * Copyright (c) 2020 Dasutein
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * See more: https://github.com/ddasutein/AutoRename/blob/master/LICENSE
+ * 
+ */
+
 /* References
 * Dataset => https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
 * My post => https://stackoverflow.com/questions/57566463/clarification-on-i18n-implementation-for-chromium-extensions
@@ -8,23 +22,5 @@ const elements = document.querySelectorAll('[data-i18n]');
 function SetLocale() {
     elements.forEach(el => el.innerHTML = chrome.i18n.getMessage(el.dataset.i18n));
 }
-
-// function SetLocale() {
-//     document.getElementById("settings_developed_by_dasutein").innerHTML = chrome.i18n.getMessage("settings_developed_by_dasutein");
-//     document.getElementById("settings_github_link").innerHTML = chrome.i18n.getMessage("settings_github_link");
-//     document.getElementById("settings_label_general_settings").innerHTML = chrome.i18n.getMessage("settings_label_general_settings");
-//     document.getElementById("settings_general_settings_show_download_folder").innerHTML = chrome.i18n.getMessage("settings_general_settings_show_download_folder");
-
-//     document.getElementById("settings_label_twitter").innerHTML = chrome.i18n.getMessage("settings_label_twitter");
-//     document.getElementById("settings_label_twitter_description").innerHTML = chrome.i18n.getMessage("settings_label_twitter_description");
-//     document.getElementById("settings_twitter_include_mention_symbol").innerHTML = chrome.i18n.getMessage("settings_twitter_include_mention_symbol");
-//     document.getElementById("settings_twitter_include_tweet_id").innerHTML = chrome.i18n.getMessage("settings_twitter_include_tweet_id");
-//     document.getElementById("settings_twitter_include_tweet_date").innerHTML = chrome.i18n.getMessage("settings_twitter_include_tweet_date");
-//     document.getElementById("settings_twitter_generate_string").innerHTML = chrome.i18n.getMessage("settings_twitter_generate_string");
-//     document.getElementById("settings_twitter_file_extension_type").innerHTML = chrome.i18n.getMessage("settings_twitter_file_extension_type");
-
-//     document.getElementById("button_save").innerHTML = chrome.i18n.getMessage("button_save");
-
-// }
 
 document.addEventListener("DOMContentLoaded", SetLocale);
