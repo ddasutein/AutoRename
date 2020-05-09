b541883cf6b83eb7108a5ed3fde35a2132b2dae9
ddasutein <dasutein@outlook.com>
2020-05-09 15:48:36 +0800

    Improvements to settings for options page
    The options page will load settings via the Settings.js file

--- js/Configuration/OptionsPage.js
+++ js/Configuration/OptionsPage.js
@@ -1,3 +1,92 @@
+/** MIT License
+ * 
+ * Copyright (c) 2020 Dasutein
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * See more: https://github.com/ddasutein/AutoRename/blob/master/LICENSE
+ * 
+ */
+
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        loadHTMLSettings();
+    }
+}
+document.getElementById("button_save").addEventListener("click", SaveOptions);
+
+let loadHTMLSettings = function () {
+
+    let generalPref = SettingsArray.filter(function (key) {
+        return key.category == CategoryEnum.General;
+    });
+
+    generalPref.forEach(function (x) {
+        switch (x.key) {
+            case "showDownloadFolderCheckbox":
+                document.getElementById("showDownloadFolderCheckbox").checked = x.value
+                break;
+        }
+    });
+
+    let twitterPref = SettingsArray.filter(function (key) {
+        return key.category == CategoryEnum.Twitter
+    });
+
+    twitterPref.map(function (x) {
+        switch (x.key) {
+            case "showMentionSymbol":
+                document.getElementById("twitter_mention").checked = x.value;
+                break;
+            case "showTweetId":
+                document.getElementById("twitter_tweet_id").checked = x.value;
+                break;
+            case "useDate":
+                document.getElementById("IsUsingDateCheckbox").checked = x.value;
+                UseDateFormat(x.value);
+                break;
+            case "dateFormatting":
+                document.getElementById("DateFormatTypeSelect").value = x.value;
+                break;
+            case "":
+                document.getElementById("twitter_file_extension_type").value = x.value;
+                break;
+            case "fileNameStringLength":
+                document.getElementById("twitter_string_length").value = x.value;
+                break;
+        }
+    });
+
+    let lineblogPref = SettingsArray.filter(function (key) {
+        return key.category == CategoryEnum.LINE_BLOG
+    });
+
+    lineblogPref.map(function (x) {
+        switch (x.key) {
+            case "lbPrefIncludeWebsiteTitle":
+                document.getElementById("lineblog_web_title").checked = x.value;
+                break;
+            case "lbPrefIncludeBlogTitle":
+                document.getElementById("lineblog_include_title").checked = x.value;
+                break;
+            case "lbPrefUseDate":
+                document.getElementById("lineblog_IsUsingDateCheckbox").checked = x.value;
+                LineBlogUseDateFormat(x.value);
+                break;
+            case "lbPrefDateFormat":
+                document.getElementById("lineblog_date_format").value = x.value;
+                break;
+            case "lbPrefStringGenerator":
+                document.getElementById("lineblog_string_length").value = x.value;
+                break;
+        }
+    });
+}
+
 function SaveOptions() {
 
     // General Settings
@@ -17,72 +106,20 @@ function SaveOptions() {
     let lbPrefUseDate = document.getElementById("lineblog_IsUsingDateCheckbox").checked;
     let lbPrefDateFormatting = document.getElementById("lineblog_date_format").value;
     let lbPrefStringGenerator = document.getElementById("lineblog_string_length").value;
-    
-    chrome.storage.local.set({
-        // General Settings
-        showDownloadFolderCheckbox: showDownloadFolderCheckbox,
 
-        // Twitter Settings
-        fileNameStringLength: fileNameStringLength,
-        showMentionSymbol: includeMentionSymbol,
-        showTweetId: includeTweetId,
-        twitterFileExtensionType: twitterFileExtensionType,
-        useDate: useDate,
-        dateFormatting: dateFormatting,
+    SaveSettings("showDownloadFolderCheckbox", showDownloadFolderCheckbox);
+    SaveSettings("showMentionSymbol", includeMentionSymbol);
+    SaveSettings("showTweetId", includeTweetId);
+    SaveSettings("fileNameStringLength", fileNameStringLength);
+    SaveSettings("useDate", useDate);
+    SaveSettings("dateFormatting", dateFormatting);
+    SaveSettings("lbPrefIncludeWebsiteTitle", lbPrefIncludeWebsiteTitle);
+    SaveSettings("lbPrefIncludeBlogTitle", lbPrefIncludeBlogTitle);
+    SaveSettings("lbPrefUseDate", lbPrefUseDate);
+    SaveSettings("lbPrefDateFormat", lbPrefDateFormatting);
+    SaveSettings("lbPrefStringGenerator", lbPrefStringGenerator);
 
-        // LINE BLOG
-        lbPrefIncludeWebsiteTitle: lbPrefIncludeWebsiteTitle,
-        lbPrefIncludeBlogTitle: lbPrefIncludeBlogTitle,
-        lbPrefUseDate: lbPrefUseDate,
-        lbPrefDateFormat: lbPrefDateFormatting,
-        lbPrefStringGenerator: lbPrefStringGenerator
-
-    }, function () {
-        alert(chrome.i18n.getMessage("settings_save_success"));
-    });
-}
-
-function LoadOptions() {
-    chrome.storage.local.get({
-        // General Settings
-        showDownloadFolderCheckbox: false,
-
-        // Twitter Settings
-        fileNameStringLength: "8",
-        showMentionSymbol: true,
-        showTweetId: true,
-        twitterFileExtensionType: ".jpg",
-        useDate: false,
-        dateFormatting: "0",
-
-        // LINE BLOG
-        lbPrefIncludeWebsiteTitle: false,
-        lbPrefIncludeBlogTitle: false,
-        lbPrefUseDate: false,
-        lbPrefDateFormat: "0",
-        lbPrefStringGenerator: "4"
-
-    }, function (items) {
-        // General Settings
-        document.getElementById("showDownloadFolderCheckbox").checked = items.showDownloadFolderCheckbox;
-
-        // Twitter Settings
-        document.getElementById("twitter_string_length").value = items.fileNameStringLength;
-        document.getElementById("twitter_mention").checked = items.showMentionSymbol;
-        document.getElementById("twitter_tweet_id").checked = items.showTweetId;
-        document.getElementById("twitter_file_extension_type").value = items.twitterFileExtensionType;
-        document.getElementById("IsUsingDateCheckbox").checked = items.useDate;
-        document.getElementById("DateFormatTypeSelect").value = items.dateFormatting;
-        UseDateFormat(items.useDate);
-
-        // LINE BLOG Settings
-        document.getElementById("lineblog_web_title").checked = items.lbPrefIncludeWebsiteTitle;
-        document.getElementById("lineblog_include_title").checked = items.lbPrefIncludeBlogTitle;
-        document.getElementById("lineblog_IsUsingDateCheckbox").checked = items.lbPrefUseDate;
-        document.getElementById("lineblog_date_format").value = items.lbPrefDateFormat;
-        document.getElementById("lineblog_string_length").value = items.lbPrefStringGenerator;
-        LineBlogUseDateFormat(items.lbPrefUseDate);
-    });
+    alert(chrome.i18n.getMessage("settings_save_success"));
 }
 
 // #region Twitter
@@ -168,6 +205,3 @@ lineblogIsUsingDateChecked.onchange = function () {
 //     }
 // }
 
-document.addEventListener("DOMContentLoaded", LoadOptions);
-document.getElementById("button_save").addEventListener("click",
-    SaveOptions);
