e15b717e323d874bda9f9b88ca416ff5966a730e
ddasutein <dasutein@outlook.com>
2022-01-08 18:45:06 +1000

    Start of unifying options page codebase
    To simplify development of the options page, i am beginning the process of migrating them into a single codebase. That way,it will be easier to make changes

---
+++ ui/testscript.js
@@ -0,0 +1,14 @@
+console.log("POPUP HTML")
+
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.tabTarget);
+        tabContents.forEach(tabContent => tabContent.classList.remove("active"));
+        tabs.forEach(tabContent => tabContent.classList.remove("active"));
+        tab.classList.add("active");
+        target.classList.add("active");
+    });
+});
