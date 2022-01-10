ec37a0927cd2fb351be6aa887ac1e0b537822ba9
ddasutein <dasutein@outlook.com>
2022-01-11 09:25:12 +1000

    Start development of integrating Github rest api
    This is mainly to make announcement banners dynamic (WIP)

--- js/Event/Announcement.js
+++ js/Event/Announcement.js
@@ -12,4 +12,33 @@
  * 
  */
 
-document.getElementsByClassName('main-body-section-announcement')[0].style.display ='none';
+
+function loadRestAPI(){
+
+    function testCallback(){
+        if (xhr_.readyState === XMLHttpRequest.DONE){
+            if (xhr_.status == 200){
+                result = xhr_.responseText;
+                console.log("REST RESPONSE");
+                console.log(JSON.parse(result));
+                result = JSON.parse(result);
+                console.log(result.locked)
+                if (result.locked == true){
+                    document.getElementsByClassName('main-body-section-announcement')[0].style.display ='none';
+                }
+            }
+        }
+    }
+    
+
+    let xhr_ = new XMLHttpRequest();
+    xhr_.open("GET", "https://api.github.com/repos/ddasutein/AutoRename/issues/24")
+    xhr_.onreadystatechange = testCallback;
+    xhr_.send();
+}
+
+document.addEventListener("load", loadRestAPI());
+
+console.log(document.location)
+
+
