7c4d41bf428dc9a88b429112368d110e06cc19e2
Dasutein <dasutein@outlook.com>
2022-02-01 20:15:07 +0800

    Update testscript.js

--- ui/testscript.js
+++ ui/testscript.js
@@ -21,3 +21,40 @@ if (prefColorScheme.matches) {
   } else {
     document.body.classList.toggle("dark");
   }
+
+
+  document.onreadystatechange = () => {
+    if (document.readyState == "complete") {
+        // document.getElementById("button_save").addEventListener("click", SaveOptions2);
+    }
+}
+
+function testClick(btn){
+  let btnId = btn.id;
+  alert(btnId)
+}
+
+function SaveOptions2(){
+  // swal("", "This is a test dialog!", "success", {
+  //   buttons: false,
+  //   timer: 1500
+  // })
+
+  // swal({
+  //   title: "Test",
+  //   text: "Text test",
+  //   icon: "warning",
+  //   buttons: true,
+  //   dangerMode: true
+  // }).then((willDelete)=>{
+  //   if (willDelete) {
+  //     swal("Poof! Your imaginary file has been deleted!", {
+  //       icon: "success",
+  //     });
+  //   } else {
+  //     swal("Your imaginary file is safe!", {
+  //       icon: "info"
+  //     });
+  //   }
+  // })
+}
