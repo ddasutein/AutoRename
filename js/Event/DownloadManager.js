d8a9057c0b7c454c3fd29e98cdeb3f24d1f27087
Dasutein <dasutein@outlook.com>
2022-11-30 16:33:41 +0800

    [WIP] Mapping download history

--- js/Event/DownloadManager.js
+++ js/Event/DownloadManager.js
@@ -14,26 +14,26 @@
 
 var DownloadManager = {
 
-    AddDownloadQueue: ((data)=>{
+    AddDownloadQueue: ((data) => {
 
-        generalSettings = Settings.Load().General.map((data)=>{
+        generalSettings = Settings.Load().General.map((data) => {
             return {
                 "key": data.key,
                 "value": data.value
             }
-        }).reduce((obj, data)=>{
+        }).reduce((obj, data) => {
             obj[data.key] = data;
             return obj;
         }, {});
 
         let downloadData = generalSettings["global_download_queue_data"].value;
-        if (downloadData.length > 0){
+        if (downloadData.length > 0) {
             downloadData = JSON.parse(downloadData)
         } else {
             downloadData = [];
         }
 
-        data.forEach((x)=>{
+        data.forEach((x) => {
             downloadData.push({
                 filename: x.filename,
                 url: x.url,
@@ -46,33 +46,55 @@ var DownloadManager = {
 
     }),
 
-    ClearDownloadQueue: (()=>{
+    ClearDownloadQueue: (() => {
         Settings.Save("global_download_queue_data", "");
         Utility.SetBadgeText(null);
     }),
 
-    StartDownload: ((data)=>{
+    StartDownload: ((data) => {
 
-        generalSettings = Settings.Load().General.map((data)=>{
+        let fileData = {};
+
+        generalSettings = Settings.Load().General.map((data) => {
             return {
                 "key": data.key,
                 "value": data.value
             }
-        }).reduce((obj, data)=>{
+        }).reduce((obj, data) => {
             obj[data.key] = data;
             return obj;
         }, {});
-        
-        data.forEach((x)=>{
+
+        data.forEach((x) => {
             chrome.downloads.download({
                 url: x.url,
                 filename: `${x.filename}`,
                 saveAs: generalSettings["global_enable_save_as_window"].value
-            },((id)=>{
-                console.log(`download id : ${id}`)
+            }, ((id) => {
+
+                chrome.downloads.onChanged.addListener((delta) => {
+
+                    if (id == delta.id) {
+
+                        if (delta.state && delta.state.current == "in_progress") {
+                            fileData["size"] = delta.fileSize.current;
+                            fileData["url"] = delta.url.current;
+                            fileData["id"] = delta.id;
+                            fileData["name"] = x.filename;
+                        }
+
+                        if (delta.state && delta.state.current == "complete") {
+                            generalSettings["global_show_download_folder"].value ? chrome.downloads.showDefaultFolder() : null;
+                            tmp = [];
+                            tmp.push(fileData);
+                            Settings.Save("global_download_history_data", JSON.stringify(tmp));
+                        }
+                    }
+                });
+
             }));
         });
-
     }),
 
 }
+
