7e6da0c6faf4eff84b190566d813b5d7bc89e6e9
ddasutein <dasutein@outlook.com>
2023-07-15 19:41:05 +0800

    File name builder functionality and settings mapping

--- js/Threads/ThreadsContent.js
+++ js/Threads/ThreadsContent.js
@@ -14,6 +14,18 @@
 
 var Threads = {
 
+    Settings: (()=>{
+        return Settings.Load().Threads.map((data)=>{
+            return {
+                "key": data.key,
+                "value": data.value
+            }
+        }).reduce((obj, data)=>{
+            obj[data.key] = data;
+            return obj;
+        }, {})
+    }),
+
     SaveMedia: ((data, contextMenuSelectedId) => {
 
         let urlObj = {
@@ -21,7 +33,6 @@ var Threads = {
             link_url: data.link_url,
             tab_url: data.tab_url
         }
-
 
         const getImageFormat = function (mediaLink) {
 
@@ -31,48 +42,107 @@ var Threads = {
             let src;
             if (imageFormats.some(x => (mLink).includes(x))) {
                 src = mLink.split("/")[5];
-                console.log(src.lastIndexOf("."))
                 src = src.substring(src.lastIndexOf("."));
                 return src;
             }
         }
-        console.log(getImageFormat(urlObj))
-        console.log(urlObj)
-        filename = Utility.SplitURL(urlObj.tab_url, 4);
-        console.log(filename)
-        fileNameDisplay = filename;
+
+        const BuildThreadsFileName = ((threadsSettings, urlObj, include_prefix)=>{
+            
+            ThreadsAttributes = WebsiteSupport.filter((x => x.uris == "threads.net"))[0];
+            let temp = ThreadsAttributes.placeholder;
+            let user;
+            temp = temp.split("-");
+
+            threadsSettings["threadsIncludeWebsiteTitle"].value ?  temp[temp.indexOf("{website_title}")] = "Threads" : Utility.RemoveUnusedParameter(temp, "{website_title}")
+            user = urlObj.tab_url.split("/")[3];
+            threadsSettings["threadsIncludeMentionSymbol"].value == false ? user = user.replace("@", "") : null;
+            temp[temp.indexOf("{attrib1}")] = user;
+
+            if (threadsSettings["threadsIncludeDate"].value){
+                let prefObj = {};
+
+                if (threadsSettings["threadsPreferLocaleFormat"]){
+                    prefObj["prefer_locale_format"] = true;
+                    const timedateValue = getTimeDate(prefObj);
+                    temp[temp.indexOf("{date}")] = timedateValue
+                } else {
+                    prefObj["prefer_locale_format"] = false;
+
+                    if (threadsSettings["threadsDateFormat"].value == "custom"){
+                        prefObj["date_format"] = threadsSettings["threadsCustomDateFormat"];
+                    } else {
+                        prefObj["date_format"] = GetDateFormat(threadsSettings["threadsDateFormat"].value);
+                    }
+
+                    const timedateValue = getTimeDate(prefObj);
+                    temp[temp.indexOf("{date}")] = timedateValue;
+                }
+            } else {
+                Utility.RemoveUnusedParameter(temp, "{date}");
+            }
+
+            include_prefix == true ? temp[temp.indexOf("{prefix}")] = threadsSettings["threadsCustomPrefix"].value : Utility.RemoveUnusedParameter(temp, "{prefix}");
+            
+            
+            if (urlObj.tab_url.includes("post")){
+                temp[temp.indexOf("{attrib2}")] = urlObj.tab_url.split("/")[5];
+            } else {
+                Utility.RemoveUnusedParameter(temp, "{attrib2}");
+            }
+
+            temp[temp.indexOf("{randomstring}")] = Utility.GenerateRandomString(threadsSettings["threadsRandomStringLength"].value);
+
+            let finalFilePath;
+
+            if (threadsSettings["threadsSaveImageToFolderBasedOnUsername"].value == true){
+                finalFilePath = `${user}/${temp.toString().replace(/,/g, "-")}`;
+            } else {
+                finalFilePath = `${temp.toString().replace(/,/g, "-")}`;
+            }
+
+            return {
+                filename_path: finalFilePath + getImageFormat(urlObj),
+                filename_display: temp.toString().replace(/,/g, "-"),
+                title: ThreadsAttributes.name
+            }
+
+        });
 
         let threadsFileProp = [];
 
         switch (contextMenuSelectedId) {
             case contextMenuId.saveImage:
+                fl = BuildThreadsFileName(Threads.Settings(), urlObj, false);
                 threadsFileProp.push({
-                    filename: filename + getImageFormat(urlObj),
-                    filename_display: fileNameDisplay,
+                    filename: fl.filename_path,
+                    filename_display: fl.filename_display,
                     url: urlObj.info_url,
-                    website: "Threads",
+                    website: fl.title,
 
                 });
                 DownloadManager.StartDownload(threadsFileProp);
                 break;
 
             case contextMenuId.saveImageWithCustomPrefix:
+                fl = BuildThreadsFileName(Threads.Settings(), urlObj, true);
                 threadsFileProp.push({
-                    filename: filename + getImageFormat(urlObj),
-                    filename_display: fileNameDisplay,
+                    filename: fl.filename_path,
+                    filename_display: fl.filename_display,
                     url: urlObj.info_url,
-                    website: "Threads",
+                    website: fl.title,
 
                 });
                 DownloadManager.StartDownload(threadsFileProp);
                 break;
 
             case contextMenuId.addDownloadQueue:
+                fl = BuildThreadsFileName(Threads.Settings(), urlObj, false);
                 threadsFileProp.push({
-                    filename: filename + getImageFormat(urlObj),
-                    filename_display: fileNameDisplay,
+                    filename: fl.filename_path,
+                    filename_display: fl.filename_display,
                     url: urlObj.info_url,
-                    website: "Threads",
+                    website: fl.title,
 
                 });
                 DownloadManager.AddDownloadQueue(threadsFileProp);
